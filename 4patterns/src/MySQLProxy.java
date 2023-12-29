0b501ae763eed1211628fc7def0bafed914331d5
Daniil0Yehorov <114640212+Daniil0Yehorov@users.noreply.github.com>
2023-12-29 18:10:39 +0100

    Update MySQLProxy.java

--- 4patterns/src/MySQLProxy.java
+++ 4patterns/src/MySQLProxy.java
@@ -5,14 +5,14 @@ public class MySQLProxy implements  IDAO
     MySQLProxy(){
 
     }
+
     public MySQLProxy(int id_Userrole) {
         this.idUser=id_Userrole;
     }
 
     @Override
     public product InsertP(String name, float price, int amount, float age_restrictions) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.InsertP(name,price,amount,age_restrictions);}
@@ -20,8 +20,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public product_details InsertDetailsForProduct(int productId, float weight, String manufactuer, String expire_date, String form) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.InsertDetailsForProduct(productId,weight,manufactuer,expire_date,form);}
@@ -29,8 +28,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public type_product InsertTypeofProduct(int productId, String type_name) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.InsertTypeofProduct(productId,type_name);}
@@ -38,8 +36,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public ProductReview InsertProductReview(int product_Id, String reviewerName, String reviewerText, float rating, int Id) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=2){// не користувач
+        if(getRolesIdByUserId(idUser)!=2){// не користувач
             return null;
         }
         else {return dao.InsertProductReview(product_Id,reviewerName,reviewerText,rating,Id);}
@@ -47,8 +44,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public product deleteProduct(int id) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.deleteProduct(id);}
@@ -71,8 +67,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public product UpdateProduct(int id, String name, float price, int amount, float age_restrictions) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.UpdateProduct(id, name, price, amount, age_restrictions);}
@@ -80,8 +75,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public product_details UpdateProductDetails(int productId, float weight, String manufactuer, String expire_date, String form) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.UpdateProductDetails(productId, weight, manufactuer, expire_date, form);}
@@ -89,8 +83,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public type_product UpdateTypeProduct(int productId, String type_name) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.UpdateTypeProduct(productId, type_name);}
@@ -98,8 +91,7 @@ public class MySQLProxy implements  IDAO
 
     @Override
     public ProductReview UpdateProductReview(int product_Id, String reviewerName, String reviewerText, float rating, int Id) {
-        int userRoleid=getRolesIdByUserId(idUser);
-        if(userRoleid!=1){// не адмін
+        if(getRolesIdByUserId(idUser)!=1){// не адмін
             return null;
         }
         else {return dao.UpdateProductReview(product_Id, reviewerName, reviewerText, rating, Id);}
@@ -111,7 +103,7 @@ public class MySQLProxy implements  IDAO
     }
     @Override
     public User insertUser(int id, String login, String password, int role_id) {
-        return dao.insertUser(id, login, password, role_id);
+       return dao.insertUser(id, login, password, role_id);
     }
 
     @Override
