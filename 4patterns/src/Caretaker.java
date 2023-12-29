55d4e733b92b2a3558e43ace500a43a238441e70
Daniil0Yehorov <114640212+Daniil0Yehorov@users.noreply.github.com>
2023-12-29 17:46:09 +0100

    Update Caretaker.java

--- 4patterns/src/Caretaker.java
+++ 4patterns/src/Caretaker.java
@@ -1,31 +1,4 @@
-/*import java.util.ArrayList;
-import java.util.List;
-
-public class Caretaker {
-    private List<Memento> mementoList = new ArrayList();
-    private int index = 0;
-
-    public Memento previous() {
-
-        return (index > 0)?this.mementoList.get(--index):null;
-
-    }
-    public Memento next() {
-
-        return (index < this.mementoList.size())?this.mementoList.get(++index):null;
-
-    }
-
-    public void addMemento(Memento memento) {
-        if(index < this.mementoList.size())
-        {
-            for(int i = index; i < this.mementoList.size(); i++)
-                this.mementoList.remove(i);
-        }
-        this.mementoList.add(memento);
-        index++;
-    }
-}*/
+// Caretaker
 import java.util.ArrayList;
 import java.util.List;
 
